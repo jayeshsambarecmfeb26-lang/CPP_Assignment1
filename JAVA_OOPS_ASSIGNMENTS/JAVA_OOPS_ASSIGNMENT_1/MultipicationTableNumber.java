ccf9a97e2b9b4119e43ddc13eeec7f1dc6bc749e
jayeshsambarecmfeb26-lang <jayesh.sambare.cmfeb26@gmail.com>
2026-03-29 23:59:11 +0530

    Add files via upload

---
+++ JAVA_OOPS_ASSIGNMENTS/JAVA_OOPS_ASSIGNMENT_1/MultipicationTableNumber.java
@@ -0,0 +1,17 @@
+package JAVA_OOPS_ASSIGNMENT_1;
+
+// Write a program to print multiplication table of a number. 
+
+public class MultipicationTableNumber {
+
+	public static void main(String[] args)
+	{
+        int num=7;
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(num+" x "+i+" = "+(num*i));
+    }
+	
+	}
+
+}
